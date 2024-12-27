8649f8c5d7812aa51aa29f2d4053207ca20e9d62
Anas-github-acc <anas.ahamad955@gmail.com>
2024-12-28 00:51:08 +0530

    revise changes

--- v1/gunDB-chatbot/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Chat from './Chat.svelte';
-  import Header from './Header.svelte';
-</script>
-
-<div class="app" style="display: flex; flex-direction: column; height: 100vh;">
-  <Header />
-  <Chat />
-</div>``
